d1b20bfaf4e40fb9b919abd7a7d7e9018c6e2d61
marianofarrate <igorandrev@hotmail.com>
2021-08-06 10:54:03 -0300

    modificado homework 03-JS-II

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -81,13 +81,10 @@ function colors(color) {
   switch(color){
     case "blue":
       return "This is blue";
-      break;
     case "red":
       return  "This is red";
-      break;
     case "green":
       return "This is green";
-      break;
     case "orange":
       return "This is orange";
     default:
